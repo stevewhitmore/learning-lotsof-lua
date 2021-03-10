35de6b28ae19d59d2cab2bef716eabd055d9c70e
stevewhitmore <s25w89@gmail.com>
2021-03-10 10:55:19 -0600

    Localstorage working

--- src/app/lesson-quiz/lesson-quiz.component.ts
+++ src/app/lesson-quiz/lesson-quiz.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+interface QuizAnswerModel {
+  id: string;
+  answered: string;
+  correct: boolean;
+}
+
 @Component({
   selector: 'app-lesson-quiz',
   templateUrl: './lesson-quiz.component.html',
@@ -11,11 +17,12 @@ export class LessonQuizComponent implements OnChanges {
   @Output() cancelQuizClick: EventEmitter<any> = new EventEmitter();
   latestQuizContent: any;
   answerField = new FormControl();
+  answeredCorrectly: boolean = false;
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    this.latestQuizContent = changes['quizContent'].currentValue;
+    this.latestQuizContent = changes.quizContent.currentValue;
     console.log(this.latestQuizContent)
   }
 
@@ -24,13 +31,34 @@ export class LessonQuizComponent implements OnChanges {
   }
 
   submitAnswer() {
-    console.log(this.answerField.value)
+    this.answeredCorrectly = this.answerField.value === this.latestQuizContent.answer;
     this.updateLocalStorage();
   }
 
   updateLocalStorage() {
-    const storageArray = JSON.parse(localStorage.getItem('luaQuizAnswers')) || [];
-    localStorage.setItem(this.latestQuizContent.id, this.answerField.value)
+    const storageKey = 'luaQuizAnswers';
+    const storageArray = JSON.parse(localStorage.getItem(storageKey) || '[]');
+    const updatedStorageObject: QuizAnswerModel = {
+      id: this.latestQuizContent.id,
+      answered: this.answerField.value,
+      correct: this.answeredCorrectly,
+    };
+
+    if (!storageArray.some((quizResult: QuizAnswerModel) => quizResult.id === updatedStorageObject.id)) {
+      storageArray.unshift(updatedStorageObject);
+    }
+
+    const updatedStorageArray = storageArray.map((quizResult: QuizAnswerModel) => {
+      return quizResult.id === updatedStorageObject.id 
+        ? { 
+            ...quizResult, 
+            answered: updatedStorageObject.answered, 
+            correct: updatedStorageObject.correct, 
+          } 
+        : quizResult;
+    });
+
+    localStorage.setItem(storageKey, JSON.stringify(updatedStorageArray));
   }
 
 }
