68953ba16ccd65cf8aa70407b1dc3478af831678
stevewhitmore <s25w89@gmail.com>
2021-03-24 17:40:54 -0500

    Attempt at old way of service unit tests

--- src/app/shared/services/home.service.spec.ts
+++ src/app/shared/services/home.service.spec.ts
@@ -1,27 +1,38 @@
-import { fakeAsync, tick } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { of } from 'rxjs';
 import { HomeService } from '.';
 
 fdescribe('HomeService', () => {
-  let httpClientSpy: { get: jasmine.Spy };
-  let homeService: HomeService;
+    let homeService: HomeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    homeService = new HomeService(httpClientSpy as any);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HomeService],
+    });
+    homeService = TestBed.inject(HomeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should capture the contents of the README.md file (HttpClient called once)', fakeAsync(() => {
     const expectedContent = 'Some text content from the README';
+    const req = httpTestingController.expectOne('assets/README.md');
 
-    httpClientSpy.get.and.returnValue(of(expectedContent));
-
-    homeService.homeContent$.subscribe(
-        content => expect(content).toEqual(expectedContent, 'expected content'),
+    homeService.homeContent$
+        .subscribe((content: string) => {
+            expect(content).toEqual(expectedContent, 'expected content');
+            expect(req.request.method).toBe('GET');
+        },
         fail
     );
     tick();
 
-    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    req.flush(expectedContent);
   }));
 });
